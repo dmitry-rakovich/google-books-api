951c95a80545a1c15a67d3f2dc837ac3fb7f4eef
Dmitry Rakovich <dmitryrakovich@mail.ru>
2023-09-12 13:46:10 +0300

    add: routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { Route, Routes } from "react-router-dom"
+import BooksList from "./components/BooksList/BooksList"
+import Book from "./components/Book/Book"
+
 function App() {
 
   return (
     <>
-
+      <Routes>
+        <Route path="/" element={<BooksList />}/>
+        <Route path="/book/:id" element={<Book />}/>
+      </Routes>
     </>
   )
 }
---
+++ src/components/BooksList/BooksList.jsx
@@ -0,0 +1,7 @@
+const BooksList = () => {
+    return (
+        <h1>Bookslist</h1>
+    )
+}
+
+export default BooksList
