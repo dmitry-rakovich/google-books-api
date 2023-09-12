6d9773eb1e427d73847c8a53b81661c1cab71e3d
Dmitry Rakovich <dmitryrakovich@mail.ru>
2023-09-12 20:52:25 +0300

    fix: api key import, add: total count

--- src/redux/bookSLice.js
+++ src/redux/bookSLice.js
@@ -1,13 +1,14 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { API_URL } from "../constants";
-const API_KEY = import.meta.env.API_KEY
+const API_KEY = import.meta.env.VITE_API_KEY
 
 const initialState = {
     books: [],
     isLoading: false,
     error: null,
+    total: 0,
     params: {
-        q: '',
+        q: '_',
         subject: 'all',
         orderBy: 'relevance',
         startIndex: 0
@@ -22,8 +23,8 @@ export const fetchBooks = createAsyncThunk(
         orderBy = 'relevance',
         startIndex = 0
     }) => {
-        const data = await fetch(`${API_URL}?q=${q}&subject=${subject}&orderBy=${orderBy}&startIndex=${startIndex}&key=${API_KEY}`).then(response => response.json())
-        return data.items
+        const data = await fetch(`${API_URL}?q=${q}&subject=${subject}&orderBy=${orderBy}&maxResults=30&startIndex=${startIndex}&key=${API_KEY}`).then(response => response.json())
+        return data
     }
 )
 
@@ -43,8 +44,9 @@ export const bookSlice = createSlice({
         [fetchBooks.fulfilled]: (state, action) => {
             state.isLoading = false
             if(state.params.startIndex === 0) {
-                state.books = action.payload
-            } else state.books = [...state.books, ...action.payload]
+                state.books = action.payload.items
+            } else state.books = [...state.books, ...action.payload.items]
+            state.total = action.payload.totalItems
         },
         [fetchBooks.rejected]: (state, action) => {
             state.error = action.payloads
